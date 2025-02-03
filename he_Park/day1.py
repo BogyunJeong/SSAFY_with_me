e53002198dc842098d7b85225fcad7b3dafa1441
hyeeun <ha7988@naver.com>
2025-02-03 23:11:59 +0900

    day1 0203

--- he_Park/day1.py
+++ he_Park/day1.py
@@ -4,7 +4,6 @@ from collections import Counter
 
 A, B, C = map(int, input().split())
 lst = [A, B, C]
-
 number_count = dict(Counter(lst))  # {주사위 숫자:나온 개수}
 reverse = {v:k for k, v in number_count.items()}  # {나온 개수:주사위 숫자}
 
@@ -13,5 +12,6 @@ if 3 in reverse:  # 3개가 다 같다면
 
 elif 2 not in reverse:  # 3개 다 다르다면
     print(max(lst) * 100)
+    
 else:  # 2개만 같다면
     print(1000 + reverse[2] * 100)
