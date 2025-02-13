60dc6b2281b1f3378e743a3ca41cd69f5b1b0ff1
unknown <BogyunJeong>
2025-02-13 09:31:42 +0900

    2025-0213

--- day5_party.py
+++
@@ -1,9 +0,0 @@
-a,b = map(int,input().split())
-arr = list(map(int,input().split()))
-result = []
-for i in arr:
-    c = a * b
-    result.append(i - c)
-# 야호
-for i in result:
-    print(i,end = " ")
